2e995afef9a0191634f8a56e2b96a0ee50fd3438
timo.maibach <timo.maibach@bit.admin.ch>
2021-05-06 04:42:08 +0200

    add redirection

--- hackathon-frontend/src/app/form/form.component.ts
+++ hackathon-frontend/src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { MatRadioChange } from '@angular/material/radio';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,15 +11,30 @@ import { Router } from '@angular/router';
 export class FormComponent implements OnInit {
   constructor(private router: Router, private formBuilder: FormBuilder) {}
 
+  lookingForAsyl = false;
+
   infoForm = this.formBuilder.group({
     suchtAsyl: [''],
   });
 
   ngOnInit(): void {}
 
+  change(event: MatRadioChange, data) {
+    this.lookingForAsyl = true;
+  }
+
+  searchingAsyl;
+
+  changeToYes() {
+    this.searchingAsyl = true;
+  }
+
+  changeToNo() {
+    this.searchingAsyl = false;
+  }
+
   nextPage() {
-    console.log('Form data is ', this.infoForm.value);
-    if (this.infoForm.value == true) {
+    if (this.searchingAsyl == true) {
       this.router.navigateByUrl('/process');
     } else {
       this.router.navigateByUrl('/resources');
